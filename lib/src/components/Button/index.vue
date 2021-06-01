540566f1ba6086d2493f91b3424ffcb68e51ea7e
Baptiste <baptiste.mac@gmail.com>
2021-05-31 23:15:42 -0400

    Init Grics Button

---
+++ lib/src/components/Button/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <button class='btn' :style="cssVars">
+        {{ label }}
+    </button>
+</template>
+
+<script>
+export default {
+  name: 'g-button',
+  props: {
+    label: String,
+    size: String,
+    color: String,
+  },
+  computed: {
+    cssVars() {
+      return {
+        '--bg-color': this.color
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.btn {
+    border: 1px solid black;
+    padding: .5rem 1rem;
+    border-radius: 4px;
+    background-color: var(--bg-color);
+}
+</style>
