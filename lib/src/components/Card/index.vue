5549cc2ab38afd2e823e15c30fc01a0e66e270a8
Baptiste <baptiste.mac@gmail.com>
2021-05-31 23:28:43 -0400

    Card component

---
+++ lib/src/components/Card/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <article class='card'>
+        <h1>{{ title }}</h1>
+        <g-button v-bind="$props" :label="buttonLabel" />
+    </article>
+</template>
+
+<script>
+import GButton from '../Button';
+export default {
+  name: 'g-card',
+  components: { GButton },
+  props: {
+    title: String,
+    buttonLabel: {
+        type: String,
+        default: 'buttonLabel'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+    border: 1px solid black;
+    padding: 1rem;
+}
+</style>
